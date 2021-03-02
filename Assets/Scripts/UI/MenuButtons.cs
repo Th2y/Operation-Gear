d9ba58145b4a3475d9cb056e683cfc6108e33b1f
GabrielAmericoFP <72274113+GabrielAmericoFP@users.noreply.github.com>
2021-03-02 20:52:28 -0300

    - Sistema de áudio
    - Implementei o botão de iniciar direito;
    - Implementei as configurações de áudio e separei grupos num mixer;

--- Assets/Scripts/UI/MenuButtons.cs
+++ Assets/Scripts/UI/MenuButtons.cs
@@ -5,17 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
-    public AudioSource audio;
+    public float startTime = 1f;
+    public float dramaTime = 1f;
     public string SceneName;
+    public bool isMainMenu = false;
+
+    public AudioSource audio;
     public GameObject startButton;
     public GameObject settingsButton;
     public GameObject exitButton;
     public GameObject title;
     public GameObject settingsUI;
     public GameObject dramaSprite;
-    public bool isMainMenu = false;
     private void Start()
     {
+        // Dá lock do mouse na tela e começa a Coroutine da sprite de "drama"
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Confined;
         if (isMainMenu)
@@ -25,11 +29,13 @@ public class MenuButtons : MonoBehaviour
     }
     public void play()
     {
+        // Toca um áudio e pula para a Scene especificada no editor ao fim da Coroutine
         audio.Play();
         StartCoroutine(startCoroutine());
     }
     public void settings()
     {
+        // Desabilita a UI do menu principal e habilita a UI do menu de configurações
         startButton.SetActive(false);
         settingsButton.SetActive(false);
         exitButton.SetActive(false);
@@ -38,6 +44,7 @@ public class MenuButtons : MonoBehaviour
     }
     public void settingsExit()
     {
+        // Desabilita a UI do menu de configurações e habilita a UI do menu principal
         startButton.SetActive(true);
         settingsButton.SetActive(true);
         exitButton.SetActive(true);
@@ -46,28 +53,34 @@ public class MenuButtons : MonoBehaviour
     }
     public void exit()
     {
+        // Fecha o jogo
         Application.Quit();
     }
     public void highQuality()
     {
+        // Ativa as configurações de alta qualidade
         QualitySettings.SetQualityLevel(5, true);
     }
     public void mediumQuality()
     {
+        // Ativa as configurações de média qualidade
         QualitySettings.SetQualityLevel(3, true);
     }
     public void lowQuality()
     {
+        // Ativa as configurações de baixa qualidade
         QualitySettings.SetQualityLevel(0, true);
     }
     IEnumerator dramaBegin()
     {
-        yield return new WaitForSeconds(1f);
+        // Desativa a sprite de "drama" após o tempo determinado
+        yield return new WaitForSeconds(dramaTime);
         dramaSprite.SetActive(false);
     }
     IEnumerator startCoroutine()
     {
-        yield return new WaitForSeconds(1f);
+        // Muda para a Scene determinada após o tempo determinado
+        yield return new WaitForSeconds(startTime);
         SceneManager.LoadScene(this.SceneName);
     }
 }
