ffabadfcb5b57367fee58cab690bb9a05d881861
versela <69824476+versela@users.noreply.github.com>
2021-03-02 15:39:54 -0300

    Enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Animator animator;
+    private Rigidbody2D rb;
+
+    public int maxHealth = 100;
+    int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity.x > 0.01 || rb.velocity.x < -0.01)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else if(rb.velocity.x == 0f && rb.velocity.y == 0f)
+        {
+            animator.SetBool("isWalking", false);
+
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        animator.SetTrigger("Hurt");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        animator.SetBool("isDead", true);
+
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<EnemyAI>().enabled = false;
+        this.enabled = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            animator.SetTrigger("Attack");
+        }
+
+        if (collision.gameObject.CompareTag("PlayerAttack"))
+        {
+            TakeDamage(20);
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("right"))
+        {
+            transform.position += new Vector3(speed, 0, 0);
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.position += new Vector3(-speed, 0, 0);
+        }
+        if (Input.GetKey("up"))
+        {
+            transform.position += new Vector3(0, speed, 0);
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.position += new Vector3(0, -speed, 0);
+        }
+    }
+}
