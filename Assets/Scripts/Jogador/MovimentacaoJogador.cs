cba9c591225365ba7ac9d402d75b216f6791cf95
Thayane Carvalho dos Santos <56850377+Th2y@users.noreply.github.com>
2021-03-06 12:31:37 -0300

    Colocando a movimentação
    Obs: bug existente

---
+++ Assets/Scripts/Jogador/MovimentacaoJogador.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MovimentacaoJogador : MonoBehaviour
+{
+    public void Mover(DirecaoMovimento direcao)
+    {
+        switch (direcao)
+        {
+            case DirecaoMovimento.Baixo:
+                this.transform.position += Vector3.down;
+                break;
+            case DirecaoMovimento.Cima:
+                this.transform.position += Vector3.up;
+                break;
+            case DirecaoMovimento.Direita:
+                this.transform.position += Vector3.right;
+                break;
+            case DirecaoMovimento.Esquerda:
+                this.transform.position += Vector3.left;
+                break;
+        }
+    }
+}
