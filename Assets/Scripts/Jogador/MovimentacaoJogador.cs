2c4a7540a0787356af1ba371b60c3c2c4274a240
Thayane Carvalho dos Santos <56850377+Th2y@users.noreply.github.com>
2021-03-09 21:19:24 -0300

    Movimentação do player e da câmera

--- Assets/Scripts/Jogador/MovimentacaoJogador.cs
+++ Assets/Scripts/Jogador/MovimentacaoJogador.cs
@@ -4,22 +4,27 @@ public class MovimentacaoJogador : MonoBehaviour
 {
     public void Mover(DirecaoMovimento direcao)
     {
-        switch (direcao)
+        if (StatsController.instance.energy > 0)
         {
-            case DirecaoMovimento.Baixo:
-                this.transform.position += Vector3.down;
-                break;
-            case DirecaoMovimento.Cima:
-                this.transform.position += Vector3.up;
-                break;
-            case DirecaoMovimento.Direita:
-                this.transform.position += Vector3.right;
-                break;
-            case DirecaoMovimento.Esquerda:
-                this.transform.position += Vector3.left;
-                break;
-        }
+            StatsController.instance.ismove = true;
+            switch (direcao)
+            {
+                case DirecaoMovimento.Baixo:
+                    this.transform.position += Vector3.down;
+                    break;
+                case DirecaoMovimento.Cima:
+                    this.transform.position += Vector3.up;
+                    break;
+                case DirecaoMovimento.Direita:
+                    this.transform.position += Vector3.right;
+                    break;
+                case DirecaoMovimento.Esquerda:
+                    this.transform.position += Vector3.left;
+                    break;
+            }
 
-        //Tornar a variavel de clicou como true, fazendo o jogador perder estamina, e, ao diminuir a estamina, tornar a variavel como false
+            StatsController.instance.RemoveEnergy();
+            StatsController.instance.ismove = false;
+        } else Debug.Log("Sem energia");
     }
 }
---
+++ Assets/Scripts/Jogador/PlayerColetar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class PlayerColetar : MonoBehaviour
+{
+    public TextMeshProUGUI TextColetaveis;
+    public static int contarColetaveis = 0;
+    private int contadorColetaveis = 0;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Coletavel")
+        {
+            other.gameObject.SetActive(false);
+            contadorColetaveis++;
+            TextColetaveis.text = contadorColetaveis.ToString();
+            contarColetaveis = contadorColetaveis;
+        }
+    }
+}
