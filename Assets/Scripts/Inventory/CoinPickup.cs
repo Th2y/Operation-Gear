51b82f6f50df7f4f4d0549dc67841f0f10c99792
DrakenXD <79544585+DrakenXD@users.noreply.github.com>
2021-06-14 22:48:59 -0300

    Adicionando
    sistema de moeda do player

---
+++ Assets/Scripts/Inventory/CoinPickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    public AudioSource SFX_Pickup;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SFX_Pickup.Play();
+            CoinsSystem.instance.UpdateCoins();
+        }
+    }
+}
